b5e988bb785e60f849ad43e9e53a33feb0b87809
Rinmolu <rinmoluadewumi@gmail.com>
2025-08-25 19:42:48 +0100

    Commit

---
+++ Activity2.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self,maxspeed,mileage):
+        self.maxspeed = maxspeed
+        self.mileage = mileage
+Ferrari = Vehicle(349,18)
+print(Ferrari.maxspeed)
+print(Ferrari.mileage)
+BMW = Vehicle(155,15)
+print(BMW.maxspeed)
+print(BMW.mileage)
